30089c585602044afe7aacf854905a91d14baf4f
Boo <vidahora2007@gmail.com>
2026-06-12 08:19:02 -0300

    passando para outra máquina

---
+++ Angular/src/main.server.ts
@@ -0,0 +1,16 @@
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// import { config } from './app/app.config.server';
+
+// const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+// export default bootstrap;
+
+import { BootstrapContext ,bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
+
+const bootstrap = (context: BootstrapContext) => 
+  bootstrapApplication(AppComponent, config, context);
+
+export default bootstrap;
