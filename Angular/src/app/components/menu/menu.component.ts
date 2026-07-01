78ee307e2d21f5349c8c765242314375576ad411
oVinicius404 <vidahora2007@gmail.com>
2026-07-01 00:00:32 -0300

    iniciei os carrosséis responsivos, falta aperfeiçoar

--- Angular/src/app/components/menu/menu.component.ts
+++ Angular/src/app/components/menu/menu.component.ts
@@ -38,10 +38,20 @@ export class MenuComponent implements OnInit, OnDestroy, AfterViewInit {
 
   public indexAtual = 0;
   private idIntervalo: any;
+  private idIntervalo1: any;
+  private idIntervalo2: any;
 
   ngOnInit(): void {
     this.idIntervalo = setInterval(() => {
       this.proximo();
+    }, 3000);
+
+    this.idIntervalo1 = setInterval(() => {
+      this.proximo1();
+    }, 3000);
+
+    this.idIntervalo2 = setInterval(() => {
+      this.proximo2();
     }, 3000);
   }
 
@@ -53,6 +63,12 @@ export class MenuComponent implements OnInit, OnDestroy, AfterViewInit {
   ngOnDestroy(): void {
     if (this.idIntervalo) {
       clearInterval(this.idIntervalo);
+    }
+    if (this.idIntervalo1) {
+      clearInterval(this.idIntervalo1);
+    }
+    if (this.idIntervalo2) {
+      clearInterval(this.idIntervalo2);
     }
 
     document.body.style.overflow = '';
@@ -78,6 +94,56 @@ export class MenuComponent implements OnInit, OnDestroy, AfterViewInit {
       this.indexAtual--;
     } else {
       this.indexAtual = this.imagens.length - 1;
+    }
+  }
+
+  // --- DADOS DO CARROSSEL 2 (EXTRA) ---
+  public indexAtual1 = 0;
+  imagens1: string[] = [
+    'img/imagem_1.jpeg',
+    'img/imagem_2.jpeg',
+    'img/imagem_3.jpg',
+    'img/imagem_4.jpg',
+  ];
+
+  proximo1() {
+    if (this.indexAtual1 < this.imagens1.length - 1) {
+      this.indexAtual1++;
+    } else {
+      this.indexAtual1 = 0;
+    }
+  }
+
+  anterior1() {
+    if (this.indexAtual1 > 0) {
+      this.indexAtual1--;
+    } else {
+      this.indexAtual1 = this.imagens1.length - 1;
+    }
+  }
+
+  // --- DADOS DO CARROSSEL 3 (EXTRA 2) ---
+  public indexAtual2 = 0; // Novo índice independente!
+  imagens2: string[] = [
+    'img/imagem_5.jpeg',
+    'img/imagem_6.jpeg',
+    'img/imagem_7.jpeg',
+    'img/imagem_8.jpeg'
+  ];
+
+  proximo2() { // Nova função para o botão do terceiro carrossel
+    if (this.indexAtual2 < this.imagens2.length - 1) {
+      this.indexAtual2++;
+    } else {
+      this.indexAtual2 = 0;
+    }
+  }
+
+  anterior2() { // Nova função para o botão do terceiro carrossel
+    if (this.indexAtual2 > 0) {
+      this.indexAtual2--;
+    } else {
+      this.indexAtual2 = this.imagens2.length - 1;
     }
   }
 }
